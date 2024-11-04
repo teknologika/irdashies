0448b5456a1e7e485571309f9115b160b97eb93b
Tarik Alani <tarik.nzl@gmail.com>
2024-11-04 20:58:23 +1300

    add irsdk-node integration to push telemetry to renderer

--- src/bridge/iracingsdk-bridge.ts
+++ src/bridge/iracingsdk-bridge.ts
@@ -1,10 +1,31 @@
 import { BrowserWindow } from 'electron';
+import { IRacingSDK } from 'irsdk-node';
+
+const TIMEOUT = 1000;
 
 export async function publishIRacingSDKEvents(window: BrowserWindow) {
   console.log('Loading iRacing SDK bridge...');
 
-  setInterval(() => {
-    // TODO: Replace this with actual telemetry data from irsdk-node
-    window.webContents.send('telemetry', `hello world`);
-  }, 1000);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if (await IRacingSDK.IsSimRunning()) {
+      console.log('iRacing is running');
+      const sdk = new IRacingSDK();
+      sdk.autoEnableTelemetry = true;
+
+      await sdk.ready();
+
+      while (sdk.waitForData(TIMEOUT)) {
+        console.log('Data received');
+        const telemetry = sdk.getTelemetry();
+        window.webContents.send('telemetry', telemetry);
+      }
+
+      console.log('iRacing is no longer publishing telemetry');
+    } else {
+      console.log('iRacing is not running');
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
+  }
 }
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['irsdk-node'], // allows irsdk-node to import .node files
+    },
+  },
+});
