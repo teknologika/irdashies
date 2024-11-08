9b1c7c2e640caad827a921d76eed7e0b9b2fe554
Tarik Alani <tarik.nzl@gmail.com>
2024-11-08 23:10:52 +1300

    add plugin to handle native module when packed

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['irsdk-node'], // allows irsdk-node to import .node files
-    },
-  },
+  plugins: [
+    irsdkNativeModule(
+      ['node_modules/@irsdk-node/native/build/Release/irsdk_node.node'],
+      '.vite/build/node_modules/@irsdk-node/native/build/Release/'
+    ),
+  ],
 });
+
+// this handles the native module for irsdk-node so vite can bundle it as its currently cjs only
+// this plugin will import it using createRequire and copy the native module to the vite build directory
+function irsdkNativeModule(nodeFiles: string[], outDir: string) {
+  const nodeFileMap = new Map(
+    nodeFiles.map((file) => [path.basename(file), file])
+  );
+
+  return {
+    name: 'irsdk-native-module-plugin',
+    resolveId(source: string) {
+      return nodeFileMap.has(path.basename(source)) ? source : null;
+    },
+    transform(code: string, id: string) {
+      const file = nodeFileMap.get(path.basename(id));
+      if (file) {
+        return `
+          import { createRequire } from 'module';
+          const customRequire = createRequire(import.meta.url);
+          export const iRacingSdkNode = customRequire('./${file}').iRacingSdkNode;
+        `;
+      }
+      return code;
+    },
+    load(id: string) {
+      return nodeFileMap.has(path.basename(id)) ? '' : null;
+    },
+    generateBundle() {
+      nodeFileMap.forEach((fileAbs, file) => {
+        const out = `${outDir}/${file}`;
+        const nodeFile = fs.readFileSync(fileAbs);
+        fs.mkdirSync(path.dirname(out), { recursive: true });
+        fs.writeFileSync(out, nodeFile);
+      });
+    },
+  };
+}
