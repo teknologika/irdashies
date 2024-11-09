223c88804da95a836e5c6d2207acf20f40a39ab6
Tarik Alani <tarik.alani@dronedeploy.com>
2024-11-10 12:41:14 +1300

    use tray instead of dock

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, Menu } from 'electron';
+import { app, BrowserWindow, Menu, nativeImage, Tray } from 'electron';
 import path from 'path';
 import { iRacingSDKSetup } from './bridge/setup';
 import {
@@ -85,20 +85,28 @@ function trackWindowMovement(
 }
 
 function setupTaskBar() {
-  if (process.platform === 'darwin') {
-    app.dock.show();
-    app.dock.setMenu(
-      Menu.buildFromTemplate([
-        {
-          label: 'Settings',
-          click() {
-            console.log('Open settings');
-          },
-        },
-      ])
-    );
-    return;
-  }
+  // TODO: Add icon
+  const icon = nativeImage.createFromDataURL(
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAH5JREFUSEvtlUEOgCAMBJdvGfXz6rs0JuIB0zjZpFyUaxeWadlQlLxK8vnqbrBJGgKqVdJ01ajuQbC/tKwSU91vEDb0fjztK6K9pTp7BoukMbj/WZtrzSXA+exuQNHtoFF0e8jpBhTdJqAbqc7OwYcNaA6orv+XSXOAdemf/gFmjSgZ2hbq7gAAAABJRU5ErkJggg=='
+  );
+  const tray = new Tray(icon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Settings',
+      click() {
+        console.log('Open settings');
+      },
+    },
+    {
+      label: 'Quit',
+      click() {
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.setToolTip('irDashies');
+  tray.setContextMenu(contextMenu);
 }
 
 createDefaultDashboardIfNotExists();
