f8634b20475afedb8edc87fa22d977ef18d17bf2
Tarik Alani <tarik.alani@dronedeploy.com>
2024-11-10 17:06:47 +1300

    add missing file

---
+++ src/bridge/iracingSdk/dumpTelemetry.ts
@@ -0,0 +1,31 @@
+import path from 'node:path';
+import { writeFile, mkdir } from 'node:fs/promises';
+import { app } from 'electron';
+import { IRacingSDK } from 'irsdk-node';
+
+export async function dumpCurrentTelemetry() {
+  console.log('Starting...');
+  const sdk = new IRacingSDK();
+  const dataPath = app.getPath('userData');
+  const dirPath = path.join(dataPath, Date.now().toString());
+
+  await mkdir(dirPath, { recursive: true });
+
+  if (await sdk.ready()) {
+    sdk.enableLogging = true;
+    sdk.enableTelemetry(true);
+    sdk.startSDK();
+
+    if (sdk.waitForData(1000)) {
+      const telemetry = JSON.stringify(sdk.getTelemetry(), null, 2);
+      const session = JSON.stringify(sdk.getSessionData(), null, 2);
+      await Promise.all([
+        await writeFile(`${dirPath}/telemetry.json`, telemetry, 'utf-8'),
+        await writeFile(`${dirPath}/session.json`, session, 'utf-8'),
+      ]);
+      alert(`Saved to: ${dirPath}`);
+    } else {
+      alert('No telemetry data received');
+    }
+  }
+}
