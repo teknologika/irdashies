9fa79d32fb49993c676047e4ef23ff8c0f0153d0
Tarik Alani <tarik.alani@dronedeploy.com>
2024-11-18 21:42:17 +1300

    update telemetry context tests

--- src/widgets/context/TelemetryContext/TelemetryContext.spec.tsx
+++ src/widgets/context/TelemetryContext/TelemetryContext.spec.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import { TelemetryProvider, useTelemetry } from './TelemetryContext';
 import type { IrSdkBridge } from '../../../bridge/iracingSdk/irSdkBridge.type';
+import type { SessionData, TelemetryVarList } from '@irsdk-node/types';
 
 const mockBridge: IrSdkBridge = {
   onTelemetry: vi.fn(),
@@ -16,9 +17,11 @@ const TestComponent: React.FC = () => {
   return (
     <div>
       <div data-testid="telemetry">
-        {telemetry ? 'Telemetry Data' : 'No Telemetry'}
+        {telemetry ? `Brake ${telemetry.Brake?.value?.[0]}` : 'No Telemetry'}
       </div>
-      <div data-testid="session">{session ? 'Session Data' : 'No Session'}</div>
+      <div data-testid="session">
+        {session ? `Track ${session.WeekendInfo?.TrackName}` : 'No Session'}
+      </div>
     </div>
   );
 };
@@ -33,14 +36,63 @@ describe('TelemetryContext', () => {
     expect(screen.getByText('Child Component')).toBeInTheDocument();
   });
 
-  it('provides telemetry and session data', () => {
+  it('provides telemetry data', async () => {
+    // mock data being sent to mockBridge when onTelemetry and onSessionData are called
+    vi.spyOn(mockBridge, 'onTelemetry').mockImplementation((cb) =>
+      cb({
+        Brake: {
+          countAsTime: false,
+          length: 1,
+          name: 'Brake',
+          description: '0=brake released to 1=max pedal force',
+          unit: '%',
+          varType: 4,
+          value: [0.5],
+        },
+      } as TelemetryVarList)
+    );
+
     render(
       <TelemetryProvider bridge={mockBridge}>
         <TestComponent />
       </TelemetryProvider>
     );
-    expect(screen.getByTestId('telemetry')).toHaveTextContent('No Telemetry');
-    expect(screen.getByTestId('session')).toHaveTextContent('No Session');
+
+    expect(screen.getByTestId('telemetry')).toHaveTextContent('Brake 0.5');
+  });
+
+  it('provides session data', async () => {
+    // mock data being sent to mockBridge when onTelemetry and onSessionData are called
+    vi.spyOn(mockBridge, 'onSessionData').mockImplementation((cb) =>
+      cb({
+        WeekendInfo: {
+          TrackName: 'Mount Panorama',
+        },
+      } as SessionData)
+    );
+
+    render(
+      <TelemetryProvider bridge={mockBridge}>
+        <TestComponent />
+      </TelemetryProvider>
+    );
+
+    expect(screen.getByTestId('session')).toHaveTextContent(
+      'Track Mount Panorama'
+    );
+  });
+
+  it('calls onTelemetry and onSessionData when bridge is resolved', async () => {
+    render(
+      <TelemetryProvider bridge={Promise.resolve(mockBridge)}>
+        <TestComponent />
+      </TelemetryProvider>
+    );
+
+    await vi.waitFor(() => {
+      expect(mockBridge.onTelemetry).toHaveBeenCalled();
+      expect(mockBridge.onSessionData).toHaveBeenCalled();
+    });
   });
 
   it('throws error when useTelemetry is used outside of TelemetryProvider', () => {
