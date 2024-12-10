0e204c3964164823ebe89e8b7e20c44b4aa00885
Tarik Alani <tarik.alani@dronedeploy.com>
2024-12-10 21:04:44 +1300

    add wip of track map

---
+++ src/widgets/components/TrackMap/TrackMap.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { TrackMap } from './TrackMap';
+
+export default {
+  component: TrackMap,
+} as Meta;
+
+type Story = StoryObj<typeof TrackMap>;
+
+export const Primary: Story = {};
