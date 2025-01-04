ac5933087f481d6c34dbd1d77af7c82479bb5e8c
Tarik Alani <tarik.alani@dronedeploy.com>
2025-01-05 10:04:55 +1300

    add canvas implementation

---
+++ src/frontend/components/TrackMap/TrackCanvas.stories.tsx
@@ -0,0 +1,328 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { TrackCanvas, TrackDriver } from './TrackCanvas';
+import { useEffect, useState } from 'react';
+
+export default {
+  component: TrackCanvas,
+  argTypes: {
+    trackId: {
+      control: { type: 'number' },
+    },
+  },
+} as Meta;
+
+type Story = StoryObj<typeof TrackCanvas>;
+
+const sampleData = [
+  {
+    driver: {
+      CarIdx: 2,
+      CarNumber: '20',
+      CarClassColor: 16767577,
+      CarClassEstLapTime: 113.6302,
+    },
+    progress: 0.841148853302002,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 9,
+      CarNumber: '38',
+      CarClassColor: 16767577,
+      CarClassEstLapTime: 113.6302,
+    },
+    progress: 0.936923086643219,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 10,
+      CarNumber: '39',
+      CarClassColor: 16767577,
+      CarClassEstLapTime: 113.6302,
+    },
+    progress: 0.06046311929821968,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 13,
+      CarNumber: '5',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.7775947451591492,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 15,
+      CarNumber: '10',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.9761711955070496,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 16,
+      CarNumber: '13',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.9293166399002075,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 18,
+      CarNumber: '17',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.02848833240568638,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 19,
+      CarNumber: '18',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.05448886379599571,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 20,
+      CarNumber: '19',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.5834031105041504,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 22,
+      CarNumber: '22',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.48615148663520813,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 23,
+      CarNumber: '23',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.8612086772918701,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 24,
+      CarNumber: '24',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.9262315034866333,
+    isPlayer: true,
+  },
+  {
+    driver: {
+      CarIdx: 25,
+      CarNumber: '25',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.8546850681304932,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 26,
+      CarNumber: '31',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.7217929363250732,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 27,
+      CarNumber: '32',
+      CarClassColor: 16734344,
+      CarClassEstLapTime: 126.9374,
+    },
+    progress: 0.8576954007148743,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 28,
+      CarNumber: '1',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.37332478165626526,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 29,
+      CarNumber: '2',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.6421698927879333,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 30,
+      CarNumber: '3',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.9219207167625427,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 31,
+      CarNumber: '6',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.6734675765037537,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 32,
+      CarNumber: '7',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.525010883808136,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 33,
+      CarNumber: '9',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.6666178107261658,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 35,
+      CarNumber: '12',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.675060510635376,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 36,
+      CarNumber: '16',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.8526926636695862,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 37,
+      CarNumber: '27',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.7811623811721802,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 39,
+      CarNumber: '29',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.7012394070625305,
+    isPlayer: false,
+  },
+  {
+    driver: {
+      CarIdx: 40,
+      CarNumber: '30',
+      CarClassColor: 11430911,
+      CarClassEstLapTime: 126.2284,
+    },
+    progress: 0.8447133302688599,
+    isPlayer: false,
+  },
+] as TrackDriver[];
+
+export const Primary: Story = {
+  args: {
+    trackId: 1,
+    drivers: sampleData,
+  },
+};
+
+export const AllTracks: Story = {
+  render: (args) => {
+    return (
+      <div className="grid grid-cols-3 gap-2">
+        {Array.from({ length: 531 }).map((_, i) => {
+          return (
+            <div key={i}>
+              <div className="my-2">Track ID: {i + 1}</div>
+              <TrackCanvas trackId={i + 1} drivers={args.drivers} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  },
+  args: {
+    drivers: sampleData,
+  },
+};
+
+export const CirclingAround: Story = {
+  render: (args) => {
+    const [drivers, setDrivers] = useState(args.drivers);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const newDrivers = drivers.map((driver) => ({
+          ...driver,
+          progress: (driver.progress + 0.005) % 1,
+        }));
+
+        setDrivers(newDrivers);
+      }, 50);
+
+      return () => clearInterval(interval);
+    });
+
+    return <TrackCanvas trackId={args.trackId} drivers={drivers} />;
+  },
+  args: {
+    trackId: 1,
+    drivers: sampleData,
+  },
+};
---
+++ src/frontend/components/TrackMap/TrackCanvas.tsx
@@ -0,0 +1,179 @@
+import { useEffect, useRef, useState } from 'react';
+import { Driver } from '@irdashies/types';
+import tracks from './tracks/tracks.json';
+
+export type TrackProps = {
+  trackId: number;
+  drivers: TrackDriver[];
+};
+
+export type TrackDriver = {
+  driver: Driver;
+  progress: number;
+  isPlayer: boolean;
+};
+
+export interface TrackDrawing {
+  active: {
+    inside: string;
+    outside: string;
+  };
+  startFinish: {
+    line?: string;
+    arrow?: string;
+    point?: { x?: number; y?: number; length?: number } | null;
+    direction?: 'clockwise' | 'anticlockwise' | null;
+  };
+  turns?: {
+    x?: number;
+    y?: number;
+    content?: string;
+  }[];
+}
+
+export const TrackCanvas = ({ trackId, drivers }: TrackProps) => {
+  const [positions, setPositions] = useState<
+    Record<number, TrackDriver & { position: { x: number; y: number } }>
+  >({});
+  const trackDrawing = (tracks as unknown as TrackDrawing[])[trackId];
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const animationFrameIdRef = useRef<number | null>(null);
+
+  // creating an svg path so we can use getPointAtLength
+  const line = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  line.setAttribute('d', trackDrawing.active.inside || '');
+
+  useEffect(() => {
+    if (trackDrawing?.startFinish?.point?.length === undefined) return;
+    if (!drivers?.length) return;
+
+    const direction = trackDrawing.startFinish?.direction;
+    const intersectionLength = trackDrawing.startFinish?.point?.length || 0;
+    const totalLength = line.getTotalLength() || 0;
+
+    function updateCarPosition(percent: number) {
+      const adjustedLength = (totalLength * percent) % totalLength;
+      const length =
+        direction === 'anticlockwise'
+          ? (intersectionLength + adjustedLength) % totalLength
+          : (intersectionLength - adjustedLength + totalLength) % totalLength;
+      const point = line?.getPointAtLength(length);
+
+      return { x: point?.x || 0, y: point?.y || 0 };
+    }
+
+    const updatedPositions = drivers.reduce(
+      (acc, { driver, progress, isPlayer }) => {
+        const position = updateCarPosition(progress);
+        return {
+          ...acc,
+          [driver.CarIdx]: { position, driver, isPlayer, progress },
+        };
+      },
+      {} as Record<number, TrackDriver & { position: { x: number; y: number } }>
+    );
+
+    setPositions(updatedPositions);
+  }, [
+    drivers,
+    line,
+    trackDrawing.startFinish?.direction,
+    trackDrawing.startFinish?.point?.length,
+  ]);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d');
+    if (!ctx) return;
+    const scale = window.innerWidth / 1920;
+    ctx.scale(scale, scale);
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d');
+    if (!canvas || !ctx) return;
+
+    const draw = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Shadow
+      ctx.shadowColor = 'black';
+      ctx.shadowBlur = 2;
+      ctx.shadowOffsetX = 1;
+      ctx.shadowOffsetY = 1;
+
+      const inside = new Path2D(trackDrawing.active.inside);
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = 20;
+      ctx.stroke(inside);
+
+      const startFinish = new Path2D(trackDrawing.startFinish.line);
+      ctx.lineWidth = 10;
+      ctx.strokeStyle = 'red';
+      ctx.stroke(startFinish);
+
+      trackDrawing.turns?.forEach((turn) => {
+        if (!turn.content || !turn.x || !turn.y) return;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = 'white';
+        ctx.font = '2rem sans-serif';
+        ctx.fillText(turn.content, turn.x, turn.y);
+      });
+
+      Object.values(positions)
+        .sort((a, b) => Number(a.isPlayer) - Number(b.isPlayer))
+        .forEach(({ driver, position, isPlayer }) => {
+          ctx.fillStyle = isPlayer ? 'blue' : 'green';
+          ctx.beginPath();
+          ctx.arc(position.x, position.y, 40, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.fillStyle = 'white';
+          ctx.font = '2rem sans-serif';
+          ctx.fillText(driver.CarNumber, position.x, position.y);
+        });
+    };
+
+    // Animation loop
+    const animate = () => {
+      draw();
+      animationFrameIdRef.current = requestAnimationFrame(animate);
+    };
+
+    // Start animation
+    animate();
+
+    // Cleanup on component unmount
+    return () => {
+      if (!animationFrameIdRef.current) return;
+      cancelAnimationFrame(animationFrameIdRef.current);
+    };
+  }, [
+    positions,
+    trackDrawing.active.inside,
+    trackDrawing.startFinish.line,
+    trackDrawing.turns,
+  ]);
+
+  if (!trackDrawing?.active?.inside) {
+    return <>Track map unavailable</>;
+  }
+
+  return (
+    <>
+      {!trackDrawing?.startFinish?.point && (
+        <p className="text-sm">Track start point unavailable</p>
+      )}
+      <div className="overflow-hidden">
+        <canvas
+          className="will-change-transform"
+          width={1920}
+          height={1080}
+          ref={canvasRef}
+        ></canvas>
+      </div>
+    </>
+  );
+};
