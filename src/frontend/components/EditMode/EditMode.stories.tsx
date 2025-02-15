13248248753f7a30e5053efd4ea79d80df07947b
Tarik Alani <tarik.alani@dronedeploy.com>
2025-02-16 11:51:31 +1300

    fix edit mode to take full width/height of overlay

--- src/frontend/components/EditMode/EditMode.stories.tsx
+++ src/frontend/components/EditMode/EditMode.stories.tsx
@@ -2,32 +2,69 @@ import { Meta } from '@storybook/react';
 import { EditMode } from './EditMode';
 import { DashboardProvider } from '@irdashies/context';
 import type { DashboardBridge } from '@irdashies/types';
+import { Input } from '../Input';
+import { TelemetryDecorator } from '../../../../.storybook/telemetryDecorator';
+import { Standings } from '../Standings/Standings';
 
 const meta: Meta<typeof EditMode> = {
   component: EditMode,
+  decorators: [TelemetryDecorator()],
 };
 export default meta;
 
+const mockBridge: (editMode: boolean) => DashboardBridge = (editMode) => ({
+  saveDashboard: () => {
+    // noop
+  },
+  dashboardUpdated: () => {
+    // noop
+  },
+  reloadDashboard: () => {
+    // noop
+  },
+  onEditModeToggled: (callback) => {
+    callback(editMode);
+  },
+});
+
 export const Primary = {
   render: (args: { editMode: boolean }) => {
-    const mockBridge: DashboardBridge = {
-      saveDashboard: () => {
-        // noop
-      },
-      dashboardUpdated: () => {
-        // noop
-      },
-      reloadDashboard: () => {
-        // noop
-      },
-      onEditModeToggled: (callback) => {
-        callback(args.editMode);
-      },
-    };
     return (
-      <DashboardProvider bridge={mockBridge}>
+      <DashboardProvider bridge={mockBridge(args.editMode)}>
         <EditMode>
           <div className="h-20">Some content</div>
+        </EditMode>
+      </DashboardProvider>
+    );
+  },
+  args: {
+    editMode: true,
+  },
+};
+
+export const WithInput = {
+  render: (args: { editMode: boolean }) => {
+    return (
+      <div className="h-[80px] w-[400px]">
+        <DashboardProvider bridge={mockBridge(args.editMode)}>
+          <EditMode>
+            <Input />
+          </EditMode>
+        </DashboardProvider>
+      </div>
+    );
+  },
+  args: {
+    editMode: true,
+  },
+};
+
+export const WithStandings = {
+  render: (args: { editMode: boolean }) => {
+    return (
+      <DashboardProvider bridge={mockBridge(args.editMode)}>
+        <EditMode>
+          <Standings />
         </EditMode>
       </DashboardProvider>
     );
--- src/frontend/components/EditMode/EditMode.tsx
+++ src/frontend/components/EditMode/EditMode.tsx
@@ -6,7 +6,7 @@ export const EditMode = ({ children }: PropsWithChildren) => {
   const { editMode } = useDashboard();
   if (editMode) {
     return (
-      <div className="relative">
+      <div className="relative w-full h-full">
         <div className="animate-pulse-border z-20 absolute w-full h-full border-solid border-2 border-sky-500 cursor-move">
           <div className="flex items-center gap-2 absolute top-0 right-0 py-1 px-2 bg-sky-500 text-white cursor-move">
             <Resize /> <span>Edit Mode</span>
