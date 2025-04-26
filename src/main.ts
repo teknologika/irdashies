b794abfa700569f5d3803f5a9de2aedffcdab759
Tarik Alani <tarik.alani@dronedeploy.com>
2025-04-27 09:18:26 +1200

    add auto updater

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { iRacingSDKSetup } from './app/bridge/iracingSdk/setup';
 import { getOrCreateDefaultDashboard } from './app/storage/dashboards';
 import { setupTaskbar } from './app';
 import { publishDashboardUpdates } from './app/bridge/dashboard/dashboardBridge';
-
-// @ts-expect-error no types for squirrel
-import started from 'electron-squirrel-startup';
 import { TelemetrySink } from './app/bridge/iracingSdk/telemetrySink';
 import { OverlayManager } from './app/overlayManager';
+import { updateElectronApp } from 'update-electron-app';
+// @ts-expect-error no types for squirrel
+import started from 'electron-squirrel-startup';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) app.quit();
+
+updateElectronApp();
 
 const overlayManager = new OverlayManager();
 const telemetrySink = new TelemetrySink();
